57e28e072f9dbf4bf07e303e57a72120ea6de1b6
Golam Rabby <65681341+Golam-Rabby821@users.noreply.github.com>
2025-03-24 06:06:37 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const bodyParser = require('body-parser')
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +22,23 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', (req, res) => {
+
+  console.log(req.body.url)
+  let urlObject = new URL(req.body.url)
+  console.log(urlObject)
+
+  if(urlObject.protocol === 'https:'){
+    let shortend_url = Math.floor(Math.random()*10000).toString();
+    res.json({
+      original_url: urlObject.href ,
+      short_url: shortend_url
+    })
+  }else{
+    res.json({ error: 'invalid url'})
+  }
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
